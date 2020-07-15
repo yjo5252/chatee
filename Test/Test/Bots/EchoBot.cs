edface74905976ce68ca839876c1288345a8066f
Yoojin99 <crm05265@gmail.com>
2020-07-15 15:52:58 +0900

    QnA 봇과 WaterfallDialog 합치기 성공(Test에서)

--- Test/Test/Bots/EchoBot.cs
+++ Test/Test/Bots/EchoBot.cs
@@ -9,200 +9,84 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
-using Microsoft.Recognizers.Text;
-using Microsoft.Recognizers.Text.DateTime;
-using Microsoft.Recognizers.Text.Number;
+//using Microsoft.Recognizers.Text;
+//using Microsoft.Recognizers.Text.DateTime;
+//using Microsoft.Recognizers.Text.Number;
+using Microsoft.Extensions.Logging;
+using Microsoft.Bot.Builder.Dialogs;
+using Test.Dialogs;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
 
 namespace Test.Bots
 {
-    public class EchoBot : ActivityHandler
+    public class EchoBot<T> : ActivityHandler where T : Dialog
     {
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        private readonly BotState _userState;
-        private readonly BotState _conversationState;
+        protected readonly Dialog Dialog;
+        protected readonly BotState ConversationState;
+        protected readonly BotState UserState;
+        protected readonly ILogger Logger;
 
-        public EchoBot(ConversationState conversationState, UserState userState) {
+        public EchoBot(IConfiguration configuration, IHttpClientFactory httpClientFactory, ConversationState conversationState, UserState userState, T dialog, ILogger<EchoBot<T>> logger) {
+            _configuration = configuration;
+            _httpClientFactory = httpClientFactory;
 
-            _conversationState = conversationState;
-            _userState = userState;
+            ConversationState = conversationState;
+            UserState = userState;
+            Dialog = dialog;
+            Logger = logger;
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default) {
+
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            await ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await UserState.SaveChangesAsync(turnContext, false, cancellationToken);
+
         }
 
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+            var httpClient = _httpClientFactory.CreateClient();
 
-            var conversationStateAccessors = _conversationState.CreateProperty<ConversationFlow>(nameof(ConversationFlow));
-            var flow = await conversationStateAccessors.GetAsync(turnContext, () => new ConversationFlow(), cancellationToken);
+            var qnaMaker = new QnAMaker(new QnAMakerEndpoint
+                {
+                    KnowledgeBaseId = _configuration["QnAKnowledgebaseId"],
+                    EndpointKey = _configuration["QnAEndpointKey"],
+                    Host = _configuration["QnAEndpointHostName"]
+                },
+            null,
+            httpClient);
 
-            var userStateAccessors = _userState.CreateProperty<UserProfile>(nameof(UserProfile));
-            var profile = await userStateAccessors.GetAsync(turnContext, () => new UserProfile(), cancellationToken);
+            Logger.LogInformation("Running dialog with Message Activity.");
 
-            await FillOutUserProfileAsync(flow, profile, turnContext, cancellationToken);
-
-            await _conversationState.SaveChangesAsync(turnContext, false, cancellationToken);
-            await _userState.SaveChangesAsync(turnContext, false, cancellationToken);
-            /*
-            var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
-            */
-
-            
-            
-        }
-
-        private static async Task FillOutUserProfileAsync(ConversationFlow flow, UserProfile profile, ITurnContext turnContext, CancellationToken cancellationToken) {
-
-            var input = turnContext.Activity.Text?.Trim();
-            string message;
-
-            switch (flow.LastQuestionAsked) {
-                case ConversationFlow.Question.None:
-                    await turnContext.SendActivityAsync("Let's get started. name?", null, null, cancellationToken);
-                    flow.LastQuestionAsked = ConversationFlow.Question.Name;
-                    break;
-                case ConversationFlow.Question.Name:
-                    if (ValidateName(input, out var name, out message))
-                    {
-                        profile.Name = name;
-                        await turnContext.SendActivityAsync($"Hi {profile.Name}.", null, null, cancellationToken);
-                        await turnContext.SendActivityAsync("How old are you?", null, null, cancellationToken);
-                        flow.LastQuestionAsked = ConversationFlow.Question.Age;
-                        break;
-                    }
-                    else {
-                        await turnContext.SendActivityAsync(message ?? "I'm sorry, I didn't understand.", null, null, cancellationToken);
-                        break;
-                    }
-                case ConversationFlow.Question.Age:
-                    if (ValidateAge(input, out var age, out message))
-                    {
-                        profile.Age = age;
-                        await turnContext.SendActivityAsync($"I have your age as {profile.Age}.", null, null, cancellationToken);
-                        await turnContext.SendActivityAsync("When is your flight?", null, null, cancellationToken);
-                        flow.LastQuestionAsked = ConversationFlow.Question.Date;
-                        break;
-                    }
-                    else
-                    {
-                        await turnContext.SendActivityAsync(message ?? "I'm sorry, I didn't understand that.", null, null, cancellationToken);
-                        break;
-                    }
-
-                case ConversationFlow.Question.Date:
-                    if (ValidateDate(input, out var date, out message))
-                    {
-                        profile.Date = date;
-                        await turnContext.SendActivityAsync($"Your cab ride to the airport is scheduled for {profile.Date}.");
-                        await turnContext.SendActivityAsync($"Thanks for completing the booking {profile.Name}.");
-                        await turnContext.SendActivityAsync($"Type anything to run the bot again.");
-                        flow.LastQuestionAsked = ConversationFlow.Question.None;
-                        profile = new UserProfile();
-                        break;
-                    }
-                    else
-                    {
-                        await turnContext.SendActivityAsync(message ?? "I'm sorry, I didn't understand that.", null, null, cancellationToken);
-                        break;
-                    }
-            }
-        }
-
-        private static bool ValidateName(string input, out string name, out string message) {
-
-            name = null;
-            message = null;
-
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                message = "Please enter a name";
-            }
+            if (UserProfileDialog.tutorial == 0)
+                await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
             else {
-                name = input.Trim();
-            }
+                var options = new QnAMakerOptions { Top = 1 };
 
-            return message is null;
-        }
+                // The actual call to the QnA Maker service.
+                var response = await qnaMaker.GetAnswersAsync(turnContext, options);
 
-        private static bool ValidateAge(string input, out int age, out string message)
-        {
-            age = 0;
-            message = null;
+                var msg = "여기까지 ok";
+                await turnContext.SendActivityAsync(MessageFactory.Text(msg, msg), cancellationToken);
 
-            // Try to recognize the input as a number. This works for responses such as "twelve" as well as "12".
-            try
-            {
-                // Attempt to convert the Recognizer result to an integer. This works for "a dozen", "twelve", "12", and so on.
-                // The recognizer returns a list of potential recognition results, if any.
-
-                var results = NumberRecognizer.RecognizeNumber(input, Culture.English);
-
-                foreach (var result in results)
+                if (response != null && response.Length > 0)
                 {
-                    // The result resolution is a dictionary, where the "value" entry contains the processed string.
-                    if (result.Resolution.TryGetValue("value", out var value))
-                    {
-                        age = Convert.ToInt32(value);
-                        if (age >= 18 && age <= 120)
-                        {
-                            return true;
-                        }
-                    }
+                    await turnContext.SendActivityAsync(MessageFactory.Text(response[0].Answer), cancellationToken);
                 }
-
-                message = "Please enter an age between 18 and 120.";
-            }
-            catch
-            {
-                message = "I'm sorry, I could not interpret that as an age. Please enter an age between 18 and 120.";
-            }
-
-            return message is null;
-        }
-
-        private static bool ValidateDate(string input, out string date, out string message)
-        {
-            date = null;
-            message = null;
-
-            // Try to recognize the input as a date-time. This works for responses such as "11/14/2018", "9pm", "tomorrow", "Sunday at 5pm", and so on.
-            // The recognizer returns a list of potential recognition results, if any.
-            try
-            {
-                var results = DateTimeRecognizer.RecognizeDateTime(input, Culture.English);
-
-                // Check whether any of the recognized date-times are appropriate,
-                // and if so, return the first appropriate date-time. We're checking for a value at least an hour in the future.
-                var earliest = DateTime.Now.AddHours(1.0);
-
-                foreach (var result in results)
+                else
                 {
-                    // The result resolution is a dictionary, where the "values" entry contains the processed input.
-                    var resolutions = result.Resolution["values"] as List<Dictionary<string, string>>;
-
-                    foreach (var resolution in resolutions)
-                    {
-                        // The processed input contains a "value" entry if it is a date-time value, or "start" and
-                        // "end" entries if it is a date-time range.
-                        if (resolution.TryGetValue("value", out var dateString)
-                            || resolution.TryGetValue("start", out dateString))
-                        {
-                            if (DateTime.TryParse(dateString, out var candidate)
-                                && earliest < candidate)
-                            {
-                                date = candidate.ToShortDateString();
-                                return true;
-                            }
-                        }
-                    }
+                    await turnContext.SendActivityAsync(MessageFactory.Text("No QnA Maker answers were found."), cancellationToken);
                 }
-
-                message = "I'm sorry, please enter a date at least an hour out.";
             }
-            catch
-            {
-                message = "I'm sorry, I could not interpret that as an appropriate date. Please enter a date at least an hour out.";
-            }
-            return false;
         }
+
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
@@ -212,8 +96,24 @@ namespace Test.Bots
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
                     await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                    await DisplayOptionsAsync(turnContext, cancellationToken);
                 }
             }
         }
+
+        private static async Task DisplayOptionsAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            // Create a HeroCard with options for the user to interact with the bot.
+            var card = new HeroCard
+            {
+                Title = "Hi!",
+                Images = new List<CardImage> { new CardImage("https://w7.pngwing.com/pngs/271/1006/png-transparent-power-strength-gym-fitness-centre-physical-fitness-bodybuilding-physical-strength-workout-boxing-glove-logo-fictional-character.png") } 
+
+            };
+
+            var reply = MessageFactory.Attachment(card.ToAttachment());
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+
     }
 }
