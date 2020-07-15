3a392d95fa2207015e1ed4e9b7c2091f412c11f0
yjo5252 <gracejo0227@gmail.com>
2020-07-15 15:51:56 +0900

    userProfile 정보 수집

--- healtheeqna-bot-src/Dialog/RootDialog.cs
+++ healtheeqna-bot-src/Dialog/RootDialog.cs
@@ -67,13 +67,13 @@ namespace Microsoft.BotBuilderSamples.Dialog
             // This array defines how the Waterfall will execute.
             var waterfallSteps = new WaterfallStep[]
             {
-                TransportStepAsync,
-                NameStepAsync,
-                NameConfirmStepAsync,
-                AgeStepAsync,
-                PictureStepAsync,
-                ConfirmStepAsync,
-                SummaryStepAsync,
+               NameStepAsync,
+               TargetStepAsync,
+               KnowHowStepAsync,
+               PreWeightStepAsync,
+               PostWeightStepAsync,
+               DateStepAsync,
+               ConfirmStepAsync,
             };
 
             // Add named dialogs to the DialogSet. These names are saved in the dialog state.
@@ -82,7 +82,6 @@ namespace Microsoft.BotBuilderSamples.Dialog
             AddDialog(new NumberPrompt<int>(nameof(NumberPrompt<int>), AgePromptValidatorAsync));
             AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
             AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
-            AddDialog(new AttachmentPrompt(nameof(AttachmentPrompt), PicturePromptValidatorAsync));
 
             // The initial child Dialog to run.
             InitialDialogId = nameof(WaterfallDialog);
@@ -112,175 +111,99 @@ namespace Microsoft.BotBuilderSamples.Dialog
             return await stepContext.EndDialogAsync(null, cancellationToken);
         }
 
-
-        private static async Task<DialogTurnResult> TransportStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private static async Task<DialogTurnResult> NameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
             // Running a prompt here means the next WaterfallStep will be run when the user's response is received.
+
+            return await stepContext.PromptAsync(nameof(TextPrompt),
+               new PromptOptions
+               {
+                   Prompt = MessageFactory.Text("여러분과 함께 운동할 Healthee입니다! 이름을 알려주세요?"),
+               }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> TargetStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["UserName"] = ((string)stepContext.Result);
+
+            return await stepContext.PromptAsync(nameof(ChoicePrompt), new PromptOptions
+            {
+                Prompt = MessageFactory.Text("운동의 목표가 뭘까요? 다이어트, 근력, 유연성 중 하나를 골라주세요!"),
+                Choices = ChoiceFactory.ToChoices(new List<string> { "다이어트", "근력", "근력" }),
+            }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> KnowHowStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["Target"] = ((FoundChoice)stepContext.Result).Value;
+
             return await stepContext.PromptAsync(nameof(ChoicePrompt),
-                new PromptOptions
-                {
-                    Prompt = MessageFactory.Text("Please enter your mode of transport."),
-                    Choices = ChoiceFactory.ToChoices(new List<string> { "Car", "Bus", "Bicycle" }),
-                }, cancellationToken);
+               new PromptOptions
+               {
+                   Prompt = MessageFactory.Text("운동을 많이 해보셨나요? 상 중 하 중 하나를 골라주세요! "),
+                   Choices = ChoiceFactory.ToChoices(new List<string> { "상", "중", "하" }),
+               }, cancellationToken);
         }
 
-        private static async Task<DialogTurnResult> NameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> PreWeightStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            stepContext.Values["transport"] = ((FoundChoice)stepContext.Result).Value;
+            stepContext.Values["KnowHow"] = ((FoundChoice)stepContext.Result).Value;
 
-            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Text("Please enter your name.") }, cancellationToken);
+            var promptOptions = new PromptOptions
+            {
+                Prompt = MessageFactory.Text("현재 체중을 알려주세요"),
+                RetryPrompt = MessageFactory.Text("0보다 크고 300보다 작은 수치로 적어주세요."),
+            };
+
+            return await stepContext.PromptAsync(nameof(NumberPrompt<int>), promptOptions, cancellationToken);
+
+        }
+        private async Task<DialogTurnResult> PostWeightStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["PreWeight"] = (int)stepContext.Result;
+
+         
+            var promptOptions = new PromptOptions
+            {
+                Prompt = MessageFactory.Text("목표 체중을 알려주세요"),
+                RetryPrompt = MessageFactory.Text("0보다 크고 300보다 작은 수치로 적어주세요."),
+            };
+
+            return await stepContext.PromptAsync(nameof(NumberPrompt<int>), promptOptions, cancellationToken);
         }
 
-        private async Task<DialogTurnResult> NameConfirmStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> DateStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            stepContext.Values["name"] = (string)stepContext.Result;
+            stepContext.Values["PostWeight"] = (int)stepContext.Result;
 
-            // We can send messages to the user at any point in the WaterfallStep.
-            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Thanks {stepContext.Result}."), cancellationToken);
-
-            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
-            return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text("Would you like to give your age?") }, cancellationToken);
-        }
-
-        private async Task<DialogTurnResult> AgeStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        {
-            if ((bool)stepContext.Result)
+            var promptOptions = new PromptOptions
             {
-                // User said "yes" so we will be prompting for the age.
-                // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
-                var promptOptions = new PromptOptions
-                {
-                    Prompt = MessageFactory.Text("Please enter your age."),
-                    RetryPrompt = MessageFactory.Text("The value entered must be greater than 0 and less than 150."),
-                };
+                Prompt = MessageFactory.Text("목표 기간을 알려주세요"),
+                RetryPrompt = MessageFactory.Text("날짜형식으로 작성해주세요."), // 입력받은 날짜는 현재로부터 D-day 기능
+            };
 
-                return await stepContext.PromptAsync(nameof(NumberPrompt<int>), promptOptions, cancellationToken);
-            }
-            else
-            {
-                // User said "no" so we will skip the next step. Give -1 as the age.
-                return await stepContext.NextAsync(-1, cancellationToken);
-            }
-        }
+            return await stepContext.PromptAsync(nameof(NumberPrompt<int>), promptOptions, cancellationToken);
 
-        private static async Task<DialogTurnResult> PictureStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        {
-            stepContext.Values["age"] = (int)stepContext.Result;
-
-            var msg = (int)stepContext.Values["age"] == -1 ? "No age given." : $"I have your age as {stepContext.Values["age"]}.";
-
-            // We can send messages to the user at any point in the WaterfallStep.
-            await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
-
-            if (stepContext.Context.Activity.ChannelId == Bot.Connector.Channels.Msteams) //Microsoft.Bot.Builder.Dialogs.Choices
-            {
-                // This attachment prompt example is not designed to work for Teams attachments, so skip it in this case
-                await stepContext.Context.SendActivityAsync(MessageFactory.Text("Skipping attachment prompt in Teams channel..."), cancellationToken);
-                return await stepContext.NextAsync(null, cancellationToken);
-            }
-            else
-            {
-                // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
-                var promptOptions = new PromptOptions
-                {
-                    Prompt = MessageFactory.Text("Please attach a profile picture (or type any message to skip)."),
-                    RetryPrompt = MessageFactory.Text("The attachment must be a jpeg/png image file."),
-                };
-
-                return await stepContext.PromptAsync(nameof(AttachmentPrompt), promptOptions, cancellationToken);
-            }
         }
 
         private async Task<DialogTurnResult> ConfirmStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            stepContext.Values["picture"] = ((IList<System.Net.Mail.Attachment>)stepContext.Result)?.FirstOrDefault();
+            stepContext.Values["Date"] = (int)stepContext.Result;
 
-            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
-            return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text("Is this ok?") }, cancellationToken);
+            // Get the current profile object from user state.
+            var userProfile = await _userProfileAccessor.GetAsync(stepContext.Context, () => new UserProfile(), cancellationToken);
+
+            return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text("{UserProfile.Username}님과 함께 운동할 친구예요!") }, cancellationToken);
+
         }
 
-        private async Task<DialogTurnResult> SummaryStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        {
-            if ((bool)stepContext.Result)
-            {
-                // Get the current profile object from user state.
-                var userProfile = await _userProfileAccessor.GetAsync(stepContext.Context, () => new UserProfile(), cancellationToken);
-
-                userProfile.Transport = (string)stepContext.Values["transport"];
-                userProfile.Name = (string)stepContext.Values["name"];
-                userProfile.Age = (int)stepContext.Values["age"];
-              //  userProfile.Picture = (Attachment)stepContext.Values["picture"];
-
-                var msg = $"I have your mode of transport as {userProfile.Transport} and your name as {userProfile.Name}";
-
-                if (userProfile.Age != -1)
-                {
-                    msg += $" and your age as {userProfile.Age}";
-                }
-
-                msg += ".";
-
-                await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
-
-                if (userProfile.Picture != null)
-                {
-                    try
-                    {
-                        await stepContext.Context.SendActivityAsync(MessageFactory.Attachment(userProfile.Picture, "This is your profile picture."), cancellationToken);
-                    }
-                    catch
-                    {
-                        await stepContext.Context.SendActivityAsync(MessageFactory.Text("A profile picture was saved but could not be displayed here."), cancellationToken);
-                    }
-                }
-            }
-            else
-            {
-                await stepContext.Context.SendActivityAsync(MessageFactory.Text("Thanks. Your profile will not be kept."), cancellationToken);
-            }
-
-            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is the end.
-            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
-        }
 
         private static Task<bool> AgePromptValidatorAsync(PromptValidatorContext<int> promptContext, CancellationToken cancellationToken)
         {
             // This condition is our validation rule. You can also change the value at this point.
-            return Task.FromResult(promptContext.Recognized.Succeeded && promptContext.Recognized.Value > 0 && promptContext.Recognized.Value < 150);
+            return Task.FromResult(promptContext.Recognized.Succeeded && promptContext.Recognized.Value > 0 && promptContext.Recognized.Value < 300);
         }
-
-        private static async Task<bool> PicturePromptValidatorAsync(PromptValidatorContext<IList<Bot.Schema.Attachment>> promptContext, CancellationToken cancellationToken)
-        {
-            if (promptContext.Recognized.Succeeded)
-            {
-                var attachments = promptContext.Recognized.Value;
-                var validImages = new List<Bot.Schema.Attachment>();
-
-                foreach (var attachment in attachments)
-                {
-                  if (attachment.ContentType == "image/jpeg" || attachment.ContentType == "image/png")
-                    {
-                        validImages.Add(attachment);
-                    }
-                }
-
-                promptContext.Recognized.Value = validImages;
-
-                // If none of the attachments are valid images, the retry prompt should be sent.
-                return validImages.Any();
-            }
-            else
-            {
-                await promptContext.Context.SendActivityAsync("No attachments received. Proceeding without a profile picture...");
-
-                // We can return true from a validator function even if Recognized.Succeeded is false.
-                return true;
-            }
-
-
-        }
-
 
     }
 }
