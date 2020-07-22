63027b6b5b0b8a3d151b1a1f99a939160980c3e7
yjo5252 <gracejo0227@gmail.com>
2020-07-22 15:47:43 +0900

    test5-sql

---
+++ Test5-sql/SQLTest.Data/ConversationDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLTest.Data
+{
+    public class ConversationDataContext : DbContext
+    {
+        public ConversationDataContext()
+            : base("ConversationDataContextConnectionString")
+        {
+        }
+        public DbSet<Activity> Activities { get; set; }
+
+    }
+}
